1337c32caa1c4c2c922a422c3384cb1671812133
a.sotnichenko <haliivi@mail.ru>
2023-05-17 11:32:05 +0700

    1) Added pagination to search page. 2) Install module vuelidate.

--- pages/search.vue
+++ pages/search.vue
@@ -19,6 +19,24 @@
         </div>
       </div>
     </div>
+    <nav aria-label="Paginate me">
+        <ul class="pagination justify-content-center">
+            <nuxt-link v-if="previous != null" class="page-link" :to="previous" tabindex="-1">Предыдущая</nuxt-link>
+            <li v-else class="page-item disabled">
+                <a class="page-link disabled" href="#" tabindex="-1">Предыдущая</a>
+            </li>
+            <span v-for="i in total">
+                <li  v-if="current_page === i || (!$route.query.page && i === 1)" class="page-item active">
+                <nuxt-link class="page-link" :to="`?page=${i}&q=${search_query}`">{{i}}</nuxt-link></li>
+                <li v-else class="page-item">
+                <nuxt-link class="page-link" :to="`?page=${i}&q=${search_query}`">{{i}}</nuxt-link></li>
+            </span>
+            <nuxt-link v-if="next != null" class="page-link" :to="next">Следующая</nuxt-link>
+            <li v-else class="page-item disabled">
+                <a class="page-link" href="#">Следующая</a>
+            </li>
+        </ul>
+    </nav>
   </div>
 </template>
 
@@ -28,16 +46,31 @@
     export default {
         components: {SearchHeader},
         layout: "post_detail_without_search",
-        watchQuery: ['q'],
+        watchQuery: ['q', 'page'],
         data() {
             return {
-                posts: []
+                posts: [],
+                total: [],
+                next: [],
+                previous: [],
+                current_page: 0,
+                search_query: ''
             }
         },
         async asyncData({route}) {
-            const { data } = await axios.get(`http://127.0.0.1:8000/api/posts/?q=${route.query.q}`);
+            let page_and_search = route.query.page !== undefined ? `?page=${route.query.page}&q=${route.query.q}` : `?q=${route.query.q}`;
+            const { data } = await axios.get(`http://127.0.0.1:8000/api/posts/?q=${page_and_search}`);
+            let next = data.next != null ? data.next.split('/')[5] : data.next;
+            let previous = data.previous != null ? data.previous.split('/')[5] : data.previous;
+            let current_page = route.query.page;
+            let search_query = route.query.q
             return {
                 posts: data,
+                total: Math.ceil(data.count / 6),
+                next: next,
+                previous: previous,
+                current_page: Number(current_page),
+                search_query: search_query
             };
         },
     }
